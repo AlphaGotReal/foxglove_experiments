c16f94d6a0cf3d8c92af86243487e40d4e845f41
AlphaGotReal <subhampatra2411@gmail.com>
2025-10-17 04:51:50 +0530

    starting fresh

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,5 @@
-use std::thread;
-use std::time;
-
-#[derive(foxglove::Encode)]
-struct Message {
-    elapsed: f64,
-    message: String
-}
 
 fn main() {
-    foxglove::WebSocketServer::new()
-        .start_blocking()
-        .expect("Server failed to start");
 
-    loop {
-        let time = time::SystemTime::now();
-        foxglove::log!(
-            "/hello",
-            Message {
-                elapsed: time
-                    .elapsed()
-                    .expect("clock failed to elapse")
-                    .as_secs_f64(),
-                message: String::from("test")
-            }
-        );
-        thread::sleep(time::Duration::from_millis(30));
-    }
 }
+
