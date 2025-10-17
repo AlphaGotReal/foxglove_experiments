b015f4412a8de953e45694b823167337bd203ffb
AlphaGotReal <subhampatra2411@gmail.com>
2025-10-17 05:35:04 +0530

    given up for now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+use std::fs;
 
-fn main() {
+use anyhow::{Context, Result};
+use camino::Utf8Path;
+use memmap2::Mmap;
 
+fn map_mcap<P: AsRef<Utf8Path>>(p: P) -> Result<Mmap> {
+    let fd = fs::File::open(p.as_ref()).context("Couldn't open MCAP file")?;
+    unsafe { Mmap::map(&fd) }.context("Couldn't map MCAP file")
 }
 
+fn read_it() -> Result<()> {
+    let mapped = map_mcap("data/in.mcap")?;
+
+    for message in mcap::MessageStream::new(&mapped)? {
+        let msg = message?;
+        println!("message: {:?}", msg);
+        let channel = msg.channel;
+        println!("id: {}, topic: {}", channel.id, channel.topic);
+    }
+    Ok(())
+}
+
+fn main() {
+    read_it();
+}
